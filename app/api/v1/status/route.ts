24c4724a75aee86bcdd2d2a67df5159c9db66b2c
John Carlos <79121349+johncarlosofficial@users.noreply.github.com>
2024-10-02 08:11:52 -0300

    ✨ feat(api): add `GET` endpoint for database status

---
+++ app/api/v1/status/route.ts
@@ -0,0 +1,43 @@
+import database from "infra/database";
+
+// Define a named export for the GET method
+export async function GET(request: Request) {
+    const updatedAt = new Date().toISOString();
+
+    const databaseVersionResult = await database.query("SHOW server_version;");
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+    const databaseMaxConnectionsResult = await database.query(
+        "SHOW max_connections;",
+    );
+    const databaseMaxConnectionsValue =
+        databaseMaxConnectionsResult.rows[0].max_connections;
+
+    const databaseName = process.env.POSTGRES_DB;
+    const databaseOpenedConnectionsResult = await database.query({
+        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+        values: [databaseName],
+    });
+    const databaseOpenedConnectionsValue =
+        databaseOpenedConnectionsResult.rows[0].count;
+
+    // Return the response as JSON
+    return new Response(
+        JSON.stringify({
+            updated_at: updatedAt,
+            dependencies: {
+                database: {
+                    version: databaseVersionValue,
+                    max_connections: parseInt(databaseMaxConnectionsValue),
+                    opened_connections: databaseOpenedConnectionsValue,
+                },
+            },
+        }),
+        {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        },
+    );
+}
