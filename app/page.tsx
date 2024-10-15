c8feb022105ed16fef86d32772531b1511ba8b32
John Carlos <79121349+johncarlosofficial@users.noreply.github.com>
2024-10-15 10:02:49 -0300

    🔒 feat: add `(auth)` directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>;
+    return (
+        <>
+            <h1>Hello, Next.js!</h1>
+            <Link href={"/signin"}>
+                <h2>Sign In</h2>
+            </Link>
+            <Link href={"/signup"}>
+                <h2>Sign Up</h2>
+            </Link>
+        </>
+    );
 }
