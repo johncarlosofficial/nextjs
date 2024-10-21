8f8344680768ef969f2c3f0b2809904bf69b7af3
John Carlos <79121349+johncarlosofficial@users.noreply.github.com>
2024-10-21 20:25:15 +0000

    🧪 chore(tests): add `TextEncoder` support and load `.env.development` variables

--- __tests__/integration/api/v1/migrations/get.test.tsx
+++ __tests__/integration/api/v1/migrations/get.test.tsx
@@ -1,6 +1,10 @@
 import fetch from "node-fetch";
+import database from "infra/database";
 
 test("GET to /api/v1/migrations should return 200", async () => {
+    const result = await database.query({ text: "SELECT 1 + 1;" });
+    console.log(result);
+
     const response = await fetch("http://localhost:3000/api/v1/migrations");
     expect(response.status).toBe(200);
 
