e6ac8c3b19820400b17c1f063275b768bb369979
John Carlos <79121349+johncarlosofficial@users.noreply.github.com>
2024-10-25 13:04:36 -0300

    🐛 fix: SOLVE BREAKING BUG

---
+++ infra/scripts/wait-for-postgres.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { exec } = require("node:child_process");
+
+function checkPostgres(retries = 50) {
+    exec(
+        "docker exec postgres-container pg_isready --host localhost",
+        handleReturn,
+    );
+
+    function handleReturn(error: unknown, stdout: string) {
+        if (error) {
+            console.error("Error checking Postgres:", error);
+            return;
+        }
+
+        if (stdout.search("accepting connections") === -1) {
+            if (retries > 0) {
+                process.stdout.write(".");
+                checkPostgres(retries - 1);
+            } else {
+                console.error("\n🔴 Postgres did not start in time.\n");
+                process.exit(1);
+            }
+            return;
+        }
+
+        console.log("\n🟢 Postgres is ready!\n");
+    }
+}
+
+process.stdout.write("\n\n🔴 Waiting for Postgres");
+checkPostgres();
